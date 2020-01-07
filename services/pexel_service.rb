6b0c98f57e7db40e34372f9ca6a04ab2e944fb03
Mary Lang <mcl86@cornell.edu>
2020-01-07 16:51:17 -0700

    Spacing
    Co-Authored-By: David Lopez <ldavid.x.lopezl@gmail.com>
    Co-Authored-By: Scott Regenthal <freeheeling@users.noreply.github.com>

--- services/pexel_service.rb
+++ services/pexel_service.rb
@@ -1,26 +1,22 @@
 require 'faraday'
 
 class PexelService
-
   def self.get_photo(pref)
     self.get_json(pref)[:photos].sample[:url]
   end
 
   private
 
-  def self.connection(pref)
-    Faraday.new(url: 'https://api.pexels.com/v1/search?') do |f|
-      f.params[:query] = pref
-      f.headers[:Authorization] = '563492ad6f917000010000017946788e05ac45fca7d0980652b59263'
-      f.adapter Faraday.default_adapter
+    def self.connection(pref)
+      Faraday.new(url: 'https://api.pexels.com/v1/search?') do |f|
+        f.params[:query] = pref
+        f.headers[:Authorization] = '563492ad6f917000010000017946788e05ac45fca7d0980652b59263'
+        f.adapter Faraday.default_adapter
+      end
     end
 
-  end
-
-  def self.get_json(pref)
-    response = self.connection(pref).get
-    JSON.parse(response.body, symbolize_names: true)
-  end
-
+    def self.get_json(pref)
+      response = self.connection(pref).get
+      JSON.parse(response.body, symbolize_names: true)
+    end
 end
-require "pry"; binding.pry
