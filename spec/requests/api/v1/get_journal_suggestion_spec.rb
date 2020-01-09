e1708a8c2153b510624d83790c9b77d20a1db898
Mary Lang <mcl86@cornell.edu>
2020-01-09 09:41:19 -0700

    Make specs
    Co-Authored-By: David Lopez <ldavid.x.lopezl@gmail.com>
    Co-Authored-By: Scott Regenthal <freeheeling@users.noreply.github.com>
    Co-Authored-By: Tyla Devon <tyladevon@users.noreply.github.com>

--- spec/requests/api/v1/get_journal_suggestion_spec.rb
+++ spec/requests/api/v1/get_journal_suggestion_spec.rb
@@ -4,17 +4,15 @@ require 'json'
 require 'dotenv'
 Dotenv.load
 
-
 RSpec.describe 'PenpalApi' do
-  it 'receives a song suggestion request with user info' do
-    json_response = File.read('./spec/fixtures/music_resource_response.json')
+  it 'receives a journal suggestion request with user info' do
+    json_response = File.read('./spec/fixtures/journal_resource_response.json')
     user_info = {
       current_feeling: 'sad',
       feeling_preferences: ['sad', 'down'],
-      resource_preferences: ['music'],
+      resource_preferences: ['journal'],
       media_preferences: { dogs: true, cats: false, animals: true, babies: false, celestial: false, landscapes: false }}.to_json
 
     stub_request(:get, '/api/v1/suggestion').with(query: { user_info: user_info }).to_return(status: 200, body: json_response)
-
   end
 end
--- spec/requests/api/v1/get_suggestion_spec.rb
+++ spec/requests/api/v1/get_suggestion_spec.rb
@@ -4,12 +4,10 @@ require 'json'
 
 RSpec.describe 'PenpalApi' do
   it 'receives suggestion request with user info', :vcr do
-
-
     response = get "/api/v1/suggestion", {  user_info:  {
             current_feeling: 'sad',
             feeling_preferences: ['sad', 'down'],
-            resource_preferences: ['journal', 'activity', 'buddy', 'media'], 
+            resource_preferences: ['journal', 'activity', 'buddy', 'media'],
             music_preferences: ['rock', 'blues'],
             media_preferences: { dogs: true, cats: false, animals: true, babies: false, celestial: false, landscapes: false} }.to_json }, { }
 
